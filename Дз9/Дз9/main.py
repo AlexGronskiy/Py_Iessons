5d3d6238406e536991a4ed5a33f7e73ffaf5b114
AlexGronskiy <85637138+AlexGronskiy@users.noreply.github.com>
2022-08-22 19:38:11 +0300

    Add files via upload

---
+++ Дз9/Дз9/main.py
@@ -0,0 +1,55 @@
+import random
+
+print("Задание 1")
+dictionary_1 = {'a': 300, 'b': 400}
+dictionary_2 = {'c': 500, 'd': 600}
+dictionary_1.update(dictionary_2)
+print(dictionary_1)
+
+print("Задание 2")
+dictionary = {i: random.randint(1, 11) for i in range(20)}
+print(dictionary)
+result = 1
+for key in dictionary:
+    result = result * dictionary[key]
+print("Результат умножения:", result)
+
+print("Задание 3")
+dictionary = {i + 1: (i + 1)**3 for i in range(10)}
+print(dictionary)
+
+print("Задание 4")
+keys = [random.randint(-5, 5) for i in range(5)]
+values = [random.randint(-5, 5) for i in range(5)]
+print("1-ый список", keys)
+print("2-ой список", values)
+# dictionary = {i: i for i in zip(dictionary_1, dictionary_2)}
+dictionary = dict(zip(keys, values))
+print("Созданный словарь:", dictionary)
+
+print("Задание 5")
+s = 'pythonist'
+my_dict = {i: s.count(i) for i in s}
+print(my_dict)
+
+print("Задание 6")
+S = """Любіть Україну, як сонце любіть,
+як вітер, і трави, і води...
+В годину щасливу і в радості мить,
+любіть у годину негоди.
+
+Любіть Україну у сні й наяву,
+вишневу свою Україну,
+красу її, вічно живу і нову,
+і мову її солов'їну.
+
+Без неї - ніщо ми, як порох і дим,
+розвіяний в полі вітрами...
+Любіть Україну всім серцем своїм
+і всіми своїми ділами.""".lower()
+S = S.lower().replace(',', '').replace('.', '').replace(' -', '').split()
+dictionary = {word: S.count(word) for word in set(S)}
+print("Словарь (слово: кол-во):\n", dictionary)
+print("Максимальное (слово: кол-во):", max(dictionary, key=dictionary.get), ':', max(dictionary.values()))
+print("Минимальное (слово: кол-во):", min(dictionary, key=dictionary.get), ':', min(dictionary.values()))
+
