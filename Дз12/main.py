7d0aba936b9889fd7f3f857111919db51b8f5542
AlexGronskiy <85637138+AlexGronskiy@users.noreply.github.com>
2022-09-09 21:31:27 +0300

    Add files via upload

--- Дз12/main.py
+++ Дз12/main.py
@@ -2,8 +2,8 @@ import random
 
 n = int(input("Введите N для квадратной матрицы:"))
 while True:
-    if n > 2:
-        Matrix = [[random.randint(1, 5) for j in range(n)] for i in range(n)]
+    if n > 5:
+        Matrix = [[random.randint(1, 50) for j in range(n)] for i in range(n)]
 
         break
     else:
@@ -15,29 +15,39 @@ for row in Matrix:
     print(*map('{:>3d}'.format, row))
 Matrix = list(map(list, zip(*Matrix)))
 tmp_1 = [sum([abs(j) for j in Matrix[i]]) for i in range(n)]
-print(tmp_1)
-print()
-
-# сортировка столбцов по возростанию их сум
-k = 1
-while k < len(Matrix):
-    for i in range(len(Matrix) - k):
-        if tmp_1[i] > tmp_1[i + 1]:
-            tmp_1[i], tmp_1[i + 1] = tmp_1[i + 1], tmp_1[i]
-            Matrix[i], Matrix[i + 1] = Matrix[i + 1], Matrix[i]
-    k += 1
-
-# сортировка бульбошкой
-for i in range(len(Matrix)):
-    for j in range(len(Matrix) - 1):
-        if Matrix[i][j] > Matrix[i][j + 1]:
-            Matrix[i][j], Matrix[i][j + 1] = Matrix[i][j + 1], Matrix[i][j]
-    print(Matrix[i])
-print()
-lst = list(zip(*Matrix))
+print(list(tmp_1))
 
 
-print('Matrix:')
-for row in lst:
-    print(*map('{:>3d}'.format, row))
-print(tmp_1)
+def sort(m):
+    # сортировка столбцов по возростанию их сум
+    k = 1
+    while k < len(m):
+        for i in range(len(m) - k):
+            if tmp_1[i] > tmp_1[i + 1]:
+                tmp_1[i], tmp_1[i + 1] = tmp_1[i + 1], tmp_1[i]
+                Matrix[i], Matrix[i + 1] = Matrix[i + 1], Matrix[i]
+        k += 1
+    k = 1
+    while k < len(m):
+        for i in range(len(m)):
+            for j in range(len(m) - 1):
+                if i % 2:
+                    if m[i][j] > m[i][j + 1]:  # сортировка методом пузырька
+                        m[i][j], m[i][j + 1] = m[i][j + 1], m[i][j]
+                else:
+                    if m[i][j] < m[i][j + 1]:  # сортировка методом камушка
+                        m[i][j], m[i][j + 1] = m[i][j + 1], m[i][j]
+        k += 1
+
+    lst = list(zip(*m))
+    return lst
+
+
+def print_new_matrix():
+    print('New matrix:')
+    for r in sort(Matrix):
+        print(*map('{:>3d}'.format, r))
+    print(tmp_1)
+
+
+print_new_matrix()
